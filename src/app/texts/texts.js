6180f2938cf3a7f7df72c61bbe6042fe2b693fd9
kevinpicon01 <digitalpruu@gmail.com>
2024-10-02 21:51:40 -0500

    Fix

--- src/app/texts/texts.js
+++ src/app/texts/texts.js
@@ -13,7 +13,7 @@ const textos = {
         title: title,
     },
     about: {
-        description1: "En INTER-ZTA, nos especializamos en miscelánea y papelería. Ofrecemos una variedad de productos para satisfacer tus necesidades diarias. En nuestra tienda, cada detalle cuenta y nos esforzamos por brindar la mejor experiencia a nuestros clientes.",
+        description1: "NTER-ZTA es tu destino y papelería para cada ocasión. Desde nuestra creación, hemos sido la elección preferida de aquellos que buscan variedad y calidad en un solo lugar. En INTER-ZTA, nos enorgullece ofrecer una gama amplia de productos que cubren todas tus necesidades, desde artículos de papelería básicos hasta detalles especiales para cualquier evento. Nuestro compromiso es proporcionar productos que combinen funcionalidad y diseño, asegurando que siempre encuentres lo que buscas. Nuestro equipo trabaja constantemente para mantener un inventario actualizado y diverso, garantizando que puedas encontrar los productos más recientes y populares. Creemos en la importancia de cada pequeño detalle, por lo que nos esforzamos por ofrecer artículos que marquen la diferencia en tu día día. Si necesitas papelería para cualquier ocasión INTER-ZTA es tu mejor opción. ¡ Visítanos y descubre la variedad y calidad que tenemos para ti!"
     },
     contact: {
         whatsappLink: `https://wa.me/${numeroWhatsApp}?text=${mensajeWhatsApp}`,
